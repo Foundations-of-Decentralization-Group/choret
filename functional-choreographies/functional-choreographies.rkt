3baacdfc2a98cabc42ade0e408f2912d2f30ceed
Alex <asbohosi@buffalo.edu>
2024-07-23 16:18:33 -0400

    Major improvement. In working on the bug mentioned in the last commit (which was due to a half-baked implementation for merging branches), how merging and branches are implemented. In this new implementation, whenever a "branch?" form is seen, it is hidden away in a (quote-syntax ... #:local) form; this allows it to emulate the functionality of a core form (since quote-syntax is, itself, a core form); the sub-expressions for each branch of the (branch? ...) form are also local-expanded so it does not need to be done later. When a merge macro is encountered, it local-expands each of its branches entirely (by providing an empty stop-list, unlike before), and then recursively matches the sub-syntax objects in each of the resulting expansions; during this recursive matching, if two matching (quote-syntax (branch? ...) #:local) forms are encountered, then they are merged into a single (quote-syntax (branch? ...) #:local) form. After the matching phase, the matched/merged syntax object is scanned for the remaining (quote-syntax (branch? ...) #:local) forms, and expanded into the lower level operations which actually perform the branching.

--- functional-choreographies/functional-choreographies.rkt
+++ functional-choreographies/functional-choreographies.rkt
@@ -34,6 +34,10 @@
   (define (eq-process? proc1 proc2)
     (free-identifier=? proc1 proc2))
 
+  (define (local-expand-expr stx)
+    (let-values ([(expanded-stx) (local-expand stx 'expression '())])
+      expanded-stx))
+
   (define-syntax-rule (no-op)
     (lambda () (void)))
 
@@ -109,7 +113,8 @@
                         #'SEND-PROC
                         recv-proc)
                        (if (cur-process? recv-proc)
-                           #`(branch? SEND-PROC [#,label #,gexpr])
+                           #`(branch?
+                              SEND-PROC [#,label #,gexpr])
                            #f))))))]))
 
 (define-syntax (choose! stx)
@@ -118,13 +123,6 @@
      #`(begin
          (send #,(fmt-net-proc #'RECV-PROC) 'void LABEL)
          GEXPR)]))
-
-(define-syntax (branch? stx)
-  (lambda (stx)
-    (raise-syntax-error
-     #f
-     "Encountered unexpected `branch?` form!"
-     stx)))
 
 (define-for-syntax (merge-branches left-branch right-branch)
   (syntax-parse #`(#,left-branch #,right-branch)
@@ -153,34 +151,53 @@
         l-label-hash
         r-label-hash
         #:combine/key (lambda (_ a b) (cons a b))))
-     #`(found-branch? L-SEND-PROC
-                      #,@
-                      (for/list ([pair (hash->list label-union)])
-                        (let ([label (car pair)]
-                              [expr (cdr pair)])
-                          (if (pair? expr)
-                              ;; The case where a label is in both branches being merged
-                              (let* ([expr1 (car expr)]
-                                     [expr2 (cdr expr)])
-                                #`[#,label (merge #,expr1 #,expr2)])
-                              ;; The case where a label is in only one branch
-                              (begin
-                                #`[#,label #,expr])))))]))
+     (syntax-property
+      #`(quote-syntax
+         (branch?
+          L-SEND-PROC
+          #,@
+          (for/list ([pair (hash->list label-union)])
+            (let ([label (car pair)]
+                  [expr (cdr pair)])
+              (if (pair? expr)
+                  ;; The case where a label is in both branches being merged
+                  (let* ([expr1 (car expr)]
+                         [expr2 (cdr expr)])
+                    #`[#,label
+                       #,(local-expand
+                          #`(merge #,expr1 #,expr2)
+                          'expression
+                          '())])
+                  ;; The case where a label is in only one branch
+                  (begin
+                    #`[#,label #,expr])))))
+         #:local)
+      'branch
+      #t)]))
 
-(define-syntax (found-branch? stx)
-  (syntax-parse stx
-    [(_ SEND-PROC [LABEL GEXPR] ...)
-     #`(let ([recv-label (recv #,(fmt-net-proc #'SEND-PROC) 'void)])
-         (cond
-           #,@(for/list ([label (syntax->list #'(LABEL ...))]
-                         [gexpr (syntax->list #'(GEXPR ...))])
-                #`[(equal? #,label recv-label)
-                   #,gexpr])))]))
+(define-syntax (branch? stx)
+  (syntax-parse stx #:literals (branch?)
+    [(branch? SEND-PROC [LABEL GEXPR] ...)
+     (syntax-property
+      #`(quote-syntax
+         (branch?
+          SEND-PROC
+          #,@(for/list ([label (syntax->list #'(LABEL ...))]
+                        [gexpr (syntax->list #'(GEXPR ...))])
+               #`[#,label
+                  #,(local-expand-expr gexpr)]))
+         #:local)
+      'branch
+      #t)]))
 
 (define-for-syntax (match-stx left-stx right-stx)
-  (syntax-parse #`(#,left-stx #,right-stx) #:literals (branch?)
-    [((branch? _ ...) (branch? _ ...))
-     (merge-branches left-stx right-stx)]
+  (syntax-parse #`(#,left-stx #,right-stx) #:literals (quote-syntax branch?)
+    [((quote-syntax (branch? L-REST ...) #:local)
+      (quote-syntax (branch? R-REST ...) #:local))
+     #:when (and
+             (syntax-property left-stx 'branch)
+             (syntax-property left-stx 'branch))
+     (merge-branches #'(branch? L-REST ...) #'(branch? R-REST ...))]
     [((L1 L2 LR ...) (R1 R2 RR ...))
      (let ([l-list (syntax->list #'(L1 L2 LR ...))]
            [r-list (syntax->list #'(R1 R2 RR ...))])
@@ -205,7 +222,7 @@
          (raise-syntax-error
           #f
           "Cannot merge mismatched expressions."
-          #'L))]))
+          left-stx))]))
 
 (define-syntax (merge stx)
   (syntax-parse stx
@@ -214,16 +231,41 @@
              (local-expand
               #'LEFT-GEPXR
               'expression
-              (list #'branch?))]
+              '())]
             [right-gexpr^
              (local-expand
               #'RIGHT-GEXPR
               'expression
-              (list #'branch?))]
+              '())]
             [matched (match-stx left-gexpr^ right-gexpr^)])
        (or
         matched
         (raise-syntax-error #f "Failed to merge!" stx)))]))
+
+(define-for-syntax (expand-branches stx)
+  (syntax-parse stx #:literals (quote-syntax branch?)
+    [(quote-syntax (branch? SEND-PROC (LABEL EXPR) ...) #:local)
+     #`(let ([recv-label (recv #,(fmt-net-proc #'SEND-PROC) 'void)])
+         (cond
+           #,@(for/list ([label (syntax->list #'(LABEL ...))]
+                         [expr (syntax->list #'(EXPR ...))])
+                #`[(equal? recv-label #,label)
+                   #,(expand-branches
+                      expr)])))]
+    [(E ER ...)
+     #`(#,@(for/list ([expr (syntax->list #'(E ER ...))])
+             (expand-branches expr)))]
+    [_
+     stx]))
+
+(define-syntax (expand-process stx)
+  (syntax-parse stx
+    [(_ EXPR)
+     (expand-branches
+      (local-expand
+       #'EXPR
+       'expression
+       '()))]))
 
 (define-syntax (chor stx)
   (syntax-parse stx
@@ -234,7 +276,7 @@
                  #`(syntax-parameterize ([cur-process #'#,proc])
                      (define-process
                        #,(fmt-net-proc proc)
-                       GEXPR ...)
+                       (expand-process (block GEXPR ...)))
                      #f))])
          #`(define-network
              #,@GEXPRS^)))]))
