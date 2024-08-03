41266d0fa8a8774186caf60000a5f986b7d40b40
Alex <asbohosi@buffalo.edu>
2024-08-03 18:34:22 -0400

    Add helper macro for defining Choret macros.

---
+++ functional-choreographies/define-chor-syntax.rkt
@@ -0,0 +1,32 @@
+#lang racket/base
+
+(require
+ racket/stxparam
+  (for-syntax
+   racket/base
+   syntax/parse))
+
+(provide in-global-expr define/provide-chor-syntax)
+
+(define-syntax-parameter in-global-expr #f)
+
+(define-syntax (define/provide-chor-syntax stx)
+  (syntax-parse stx
+    [(_ (NAME STX) (~optional (~seq #:override OVERRIDE)) BODY ...)
+     #`(begin
+         #,(if (attribute OVERRIDE)
+               #'(provide (rename-out [NAME OVERRIDE]))
+               #'(provide NAME))
+         (define-syntax (NAME STX)
+           (syntax-case STX ()
+             [(... (_ ARG ...))
+              (if (syntax-parameter-value #'in-global-expr)
+                  (let () BODY ...)
+                  #,(if (attribute OVERRIDE)
+                        #`(syntax (#,(attribute OVERRIDE) (... (~@ ARG ...))))
+                        #'(raise-syntax-error
+                           #f
+                           (string-append
+                            "Cannot use a chor form outside a choreographic "
+                            "expression.")
+                           STX)))])))]))
